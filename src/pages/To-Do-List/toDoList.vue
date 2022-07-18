df74e477f980d0229fc0ac95e1ca06a003a29f2e
Guard-SK <david.zliechovec@gmail.com>
2022-07-18 03:59:48 +0200

    visual and methods update

--- src/pages/To-Do-List/toDoList.vue
+++ src/pages/To-Do-List/toDoList.vue
@@ -1,7 +1,8 @@
 <template>
+<main>
   <div class="container">
     <div class="header">
-      <h3>{{header}}</h3>
+      <h2>{{header}}</h2>
       <li>
         <a href="index.html">Index</a>
       </li>
@@ -9,10 +10,10 @@
         <a href="deleted.html">Deleted</a>
       </li>
     </div>
-    <div>
+    <div class="input-div">
       <div>
-        <input @keyup.enter="list.push({id: list.length+1, message: newItem, deleted: isDeleted})" type="text" placeholder="To Do..." v-model="newItem">
-        <button @click="list.push({id: list.length+1, message: newItem, deleted: isDeleted})" class="btn_add">Add To List</button>
+        <input class="input" @keyup.enter="saveItem" type="text" placeholder="To Do..." v-model="newItem">
+        <button :disabled="newItem.length === 0" @click="saveItem" class="btn_add">Add To List</button>
       </div>
       <div>
         <label>
@@ -32,6 +33,7 @@
       </ul>
     </div>
   </div>
+</main>
 </template> 
 
 <script>
@@ -47,20 +49,62 @@ export default {
       isDeleted: false,
       header: 'To Do List'
     }
+  },
+  methods: {
+    saveItem() {
+      this.list.push({id: this.list.length+1, message: this.newItem, deleted: this.isDeleted})
+      this.newItem = ''
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+  
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(53, 149, 187, 0.25), rgba(0, 22, 70, 0.75));
+  text-align: center;
+}
+
+.container {
+  background-color: rgba(255, 255, 255, 0.5);
+  min-height: 100vh;
+  padding: 15px;
+  margin-left: 15%;
+  margin-right: 15%;
+  border-radius: 20px;
+  box-shadow: 5px 5px;
+}
+
+.input-div {
+  margin: 24px;
+}
+
+.input {
+  width: 40%;
+}
+
 .header {
   margin: 30px;
+}
+
+h3 {
+  margin-top: 60px;
 }
 </style>
